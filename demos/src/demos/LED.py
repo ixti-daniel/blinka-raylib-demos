15f89bfe86ccb6b6b80b65d0434a46c2d71b01bb
ixti-daniel <daniel@ixti.fi>
2024-01-03 20:33:35 -0600

    temporary frankenstein fix
    - Installing pygame through poetry in the raspberry pi 2B was impossible , so I did that inside the environment using PIP (with the required dependencies) .
    
    - Note : On the raspberry you have to explicitly tell poetry to avoid the windows dependencies for pygame.
    
    - Included rpi-gpio in toml , I might need more dependencies soon.
    - included  numpy

---
+++ demos/src/demos/LED.py
@@ -0,0 +1,15 @@
+# I just need this led to work maaan :(
+import time
+import board
+import digitalio
+
+print("hello blinky!")
+
+led = digitalio.DigitalInOut(board.D18)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = True
+    time.sleep(0.5)
+    led.value = False
+    time.sleep(0.5)
